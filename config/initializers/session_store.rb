9045d5d4ea331af2cee054a6ffd2615f86c4292e
HT Human Tecnologies <46276431+humantechnologies@users.noreply.github.com>
2020-03-29 09:50:43 +0200

    cookies parameters adapted

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,9 +3,10 @@
 # Be sure to restart your server when you modify this file.
 
 # The key base was updated as part of 2.3
+# GT20200328 modified Key: and added expiration
 if Rails.configuration.loadbalanced_configuration
-    Rails.application.config.session_store :cookie_store, key: '_greenlight-2_3_session',
+    Rails.application.config.session_store :cookie_store, key: 'Riunioni_online_session' , expire_after: 30.days ,
         domain: ENV['GREENLIGHT_SESSION_DOMAIN'] || 'blindside-dev.com'
 else
-    Rails.application.config.session_store :cookie_store, key: '_greenlight-2_3_session'
+    Rails.application.config.session_store :cookie_store, key: 'Riunioni_online_session' , expire_after: 30.days
 end
