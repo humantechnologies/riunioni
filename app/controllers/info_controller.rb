3944cfc4e95117300aea6502b9fc939dc33adfc5
HT Human Tecnologies <46276431+humantechnologies@users.noreply.github.com>
2020-03-31 15:55:02 +0200

    Add files via upload

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,25 @@
+class InfoController < ApplicationController
+  def termini
+  end
+
+  def privacy
+  end
+
+  def cookies
+  end
+
+  def prezzi
+  end
+
+  def informazioni
+  end
+
+  def help
+  end
+
+  def faq
+  end
+
+  def contatti
+  end
+end
